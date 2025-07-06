1a72c7fce9da8a2df7729dca7a54e29d896a6173
William <william10fcb1@gmail.com>
2025-07-05 23:55:40 -0300

    Logica Pantalla Maestro Beneficios

--- WebSite/WebSite/GestionBeneficios.aspx.cs
+++ WebSite/WebSite/GestionBeneficios.aspx.cs
@@ -43,7 +43,7 @@ public partial class GestionBeneficios : System.Web.UI.Page
     protected void btnAgregar_Click(object sender, EventArgs e)
     {
         BeneficioBLL gestorBeneficio490WC = new BeneficioBLL();
-        int codigoBeneficio490WC = int.Parse(txtCodigo.Text);   
+        int codigoBeneficio490WC = int.Parse(txtCodigo.Text);
         if (!gestorBeneficio490WC.ExisteNombreBeneficioAlta(txtNombre.Text))
         {
             if (int.TryParse(txtPrecio.Text, out int precioEstrella490WC))
@@ -62,22 +62,75 @@ public partial class GestionBeneficios : System.Web.UI.Page
                         CargarBeneficios();
                         LimpiarCampos();
                     }
+                    else
+                    {
+                        ScriptManager.RegisterStartupScript(this, GetType(), "alerta", "alert('El codigo ingresado Ya lo tiene un Beneficio Existente!!!');", true);
+                    }
                 }
             }
         }
     }
     protected void btnModificar_Click(object sender, EventArgs e)
     {
+        BeneficioBLL gestorBeneficio = new BeneficioBLL();
+        int codigoBeneficio490WC = int.Parse(gvBeneficios.SelectedDataKey["CodigoBeneficio"].ToString());
+        if (!gestorBeneficio.ExisteNombreBeneficioModificar(txtNombre.Text, codigoBeneficio490WC))
+        {
+            if (int.TryParse(txtPrecio.Text, out int precioEstrella490WC))
+            {
+                if (int.TryParse(txtCantidad.Text, out int cantidadReclamo490WC))
+                {
+                    float descuentoAplicar = int.Parse(txtDescuento.Text);
+                    descuentoAplicar = descuentoAplicar / 100;
+                    string nombre490WC = txtNombre.Text;
+                    Beneficio beneficioModificado490WC = gestorBeneficio.ObtenerBeneficioPorCodigo(codigoBeneficio490WC);
+                    beneficioModificado490WC.Nombre = nombre490WC;
+                    beneficioModificado490WC.PrecioEstrella = precioEstrella490WC;
+                    beneficioModificado490WC.CantidadBeneficioReclamo = cantidadReclamo490WC;
+                    beneficioModificado490WC.DescuentoAplicar = descuentoAplicar;
+                    gestorBeneficio.Modificacion(beneficioModificado490WC);
+                    CargarBeneficios();
+                    LimpiarCampos();
+                }
+            }
+        }
     }
+
     protected void btnBorrar_Click(object sender, EventArgs e)
     {
+        BeneficioBLL gestorBeneficio = new BeneficioBLL();
+        if (gvBeneficios.SelectedDataKey != null)
+        {
+            int codigoBeneficio490WC = int.Parse(gvBeneficios.SelectedDataKey["CodigoBeneficio"].ToString());
+            if (gestorBeneficio.Baja(codigoBeneficio490WC))
+            {
+                CargarBeneficios();
+                LimpiarCampos();
+            }
+            else
+            {
+                lblMensaje.Text = "No se pudo eliminar el beneficio. Puede que esté asociado a un cliente.";
+                lblMensaje.CssClass = "validador-error";
+                lblMensaje.Visible = true;
+            }
+        }
+       
     }
     protected void btnLimpiar_Click(object sender, EventArgs e)
     {
-
+        LimpiarCampos();
     }
     protected void gvBeneficios_SelectedIndexChanged(object sender, EventArgs e)
     {
+        GridViewRow row = gvBeneficios.SelectedRow;
+
+        txtCodigo.Text = Server.HtmlDecode(row.Cells[1].Text);
+        txtNombre.Text = Server.HtmlDecode(row.Cells[2].Text);
+        txtPrecio.Text = Server.HtmlDecode(row.Cells[3].Text);
+        txtCantidad.Text = Server.HtmlDecode(row.Cells[4].Text);
+        float descuento = float.Parse(Server.HtmlDecode(row.Cells[5].Text));
+        descuento = descuento * 100;
+        txtDescuento.Text = descuento.ToString();
 
     }
 }
