63151a6fef600fce95d18e1632a1507f7c948599
Saantimuraca <saantimuraca12@gmail.com>
2025-11-08 17:30:08 -0300

    Backend permisos

---
+++ WebSite/SERVICIOS/EntidadPermisoSimple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICIOS
+{
+    public class EntidadPermisoSimple : EntidadPermiso
+    {
+        public EntidadPermisoSimple(string pNombre) : base(pNombre)
+        {
+
+        }
+    }
+}
---
+++ WebSite/SERVICIOS/LogicaPermisoCompuesto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICIOS
+{
+    public class LogicaPermisoCompuesto
+    {
+        DatosPermiso dp = new DatosPermiso();
+        public bool VerificarPermisoIncluido(EntidadPermiso permisoActual, string permiso)
+        {
+            if (permisoActual.DevolverNombrePermiso() == permiso) { return true; }
+            else
+            {
+                if (permisoActual.isComposite())
+                {
+                    List<EntidadPermiso> lista = (dp.DevolverPermsisosArbol().Find(x => x.DevolverNombrePermiso() == permisoActual.DevolverNombrePermiso()) as EntidadPermisoCompuesto).listaPermisos;
+                    foreach (EntidadPermiso p in lista)
+                    {
+                        if (VerificarPermisoIncluido(p, permiso)) return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+}
